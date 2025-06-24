161ca605f897ccd1fe320282770986c06cf56efc
SollyBunny <sollybunn@gmail.com>
2025-06-24 19:46:56 +0300

    add configdir command

--- src/commands.mjs
+++ src/commands.mjs
@@ -19,11 +19,28 @@ You can join their discord [here](<https://discord.gg/NrPcJwbcEQ>)`,
 	}],
 };
 
+commands["configdir"] = {
+	embeds: [{
+		color: 0xFFAA00,
+		title: "Config Directories",
+		description: `Never put config in your install directory, this may lead to bugs
+### On Windows
+Old: \`%appdata%\\Teeworlds\`
+New: \`%appdata%\\DDNet\`
+### On Linux:
+Old: \`~/.teeworlds\`
+New: \`~/.local/share/ddnet\`
+### On MacOS:
+Old: \`~/Library/Application Support/Teeworlds\`
+New: \`~/Library/Application Support/DDNet\``
+	}],
+};
+
 commands["help"] = {
 	embeds: [{
 		color: 0xFFAA00,
 		title: "Command List",
-		description: ["ddnet", "kog", "help"].map(i => `**\$${i}**`).join(", ")
+		description: ["ddnet", "kog", "configdir", "help"].map(i => `**\$${i}**`).join(", ")
 	}]
 }
 
